14551eb1f6148fa03f167a563fb51e0d9c461fd5
Nwosu Darlington <fritzdultimate@gmail.com>
2022-11-24 06:31:03 -0800

    interface

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
+var user1;
+user1 = {
+    name: 'Darl',
+    age: 24,
+    greet: function (phrase) {
+        console.log(phrase + ' ' + this.name);
+    }
+};
+user1.greet('Hi there - I am');
 //# sourceMappingURL=app.js.map
