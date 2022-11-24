0ed5ca8223770b4d266ab12980fb59efefc3fbce
Nwosu Darlington <fritzdultimate@gmail.com>
2022-11-24 07:38:18 -0800

    discriminated type

--- dist/app.js
+++ dist/app.js
@@ -49,4 +49,17 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    var speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log('Moving at speed: ' + speed);
+}
+moveAnimal({ type: 'horse', runningSpeed: 4400 });
 //# sourceMappingURL=app.js.map
